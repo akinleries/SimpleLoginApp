0c2f4baf637bb3595b809d2790f61466e5ac5ad4
lasisi Ezekiel <ezekielakintunde18@gmail.com>
2023-06-29 13:49:07 +0100

    fetch registerd users fixx

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,7 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
-
+import { useEffect } from "react";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchRegisteredUsers = async () => {
+    try {
+      const response = await axios.get("https://reqres.in/api/users?page=2");
+
+      setUsers([...response.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRegisteredUsers();
+  }, []);
+
   return (
     <View>
       <Text>Homeeeeeeee</Text>
